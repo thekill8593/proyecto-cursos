870b2a0a1f483229e2e9a5c64d3b0abc87946e33
Jonathan <thekill8593@hotmail.com>
2022-10-31 21:18:45 -0500

    add docs

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
+//slide library 1.0
+
 const slide = (slideSelector) => {
 
     console.log('booting slide', slideSelector);
 
+    //get wrapper from DOM
     const wrapper = document.getElementById(slideSelector);
     
+    //if wrapper does not exist then throw Error
     if (!wrapper) {
         throw Error(`DOM element ${slideSelector} does not exist`);
     }
 
+    //current slide
     let currentSlideIndex = 0;
+    //direction of the navigation
     let controlDir = "next";
 
+    //get all DOM elements from wrapper
     const btnNext = wrapper.querySelector('#btn-next');
     const btnPrev = wrapper.querySelector('#btn-prev');
     const btnBack = wrapper.querySelector('#btn-back');
@@ -20,12 +27,16 @@ const slide = (slideSelector) => {
     const slides = wrapper.getElementsByClassName('sl');
     const btnOpenSlideList = wrapper.getElementsByClassName('btn-open-slide');
     const contentModalBtns = wrapper.getElementsByClassName('content-modal-btn');
+
+    //if modal is present in DOM store ref in modal var
     const modal = document.getElementById('modal');
 
+    //check if there are buttons that can open other slides
+    //add click event to this buttons
+    //reading the property data-slide-selector of the element we know which slide to open
     if (btnOpenSlideList.length) {
         for (let i = 0; i < btnOpenSlideList.length; i++) {
             btnOpenSlideList[i].addEventListener('click', (e) => {
-                console.log(btnOpenSlideList[i]);
                 wrapper.classList.add('hide');
                 const slideToOpen = btnOpenSlideList[i].dataset.slideSelector;
                 document.getElementById(slideToOpen).classList.remove('hide');
@@ -33,6 +44,9 @@ const slide = (slideSelector) => {
         }
     }
 
+    //check if there are buttons that can open the modal window
+    //add click event to this buttons
+    //reading the property data-modal of the element we know which content to load
     if (contentModalBtns.length) {
         for (let i = 0; i < contentModalBtns.length; i++) {
             contentModalBtns[i].addEventListener('click', (e) => {
@@ -45,13 +59,17 @@ const slide = (slideSelector) => {
         }
     }
 
+    //generic method to add click event
     function addClickEventToControl(control, cb) {
         if (control && typeof cb == 'function') {
             control.addEventListener('click', cb);
         }
     }
 
+    //add click events to bottom screen controls
     function addEventListenersToControls () {
+
+        //button next (Siguiente)
         addClickEventToControl(btnNext, () => {
             if (currentSlideIndex < slides.length) {
                 currentSlideIndex++;
@@ -60,6 +78,7 @@ const slide = (slideSelector) => {
             afterEachSlide();
         });
 
+        //button prev (Anterior)
         addClickEventToControl(btnPrev,  () => {
             if (currentSlideIndex > 0) {
                 currentSlideIndex--;
@@ -68,6 +87,7 @@ const slide = (slideSelector) => {
             afterEachSlide();
         });
 
+        //button home (Icono casa)
         addClickEventToControl(btnBack, () => {
             const slideToOpen = btnBack.dataset.slideSelector;
             if (slideToOpen) {
@@ -76,6 +96,8 @@ const slide = (slideSelector) => {
             }
         });
 
+        //if modal is present then add event to the close button
+        //and remove all content in the modal
         if (modal) {
             const closeButtonModal = modal.querySelector("#modal-close-btn");
             addClickEventToControl(closeButtonModal, () => {
@@ -87,8 +109,8 @@ const slide = (slideSelector) => {
             });
         }
     }
-  
 
+    //function to render current slide
     function showCurrentActiveSlide() {
 
         //if index if out of bounds then return
@@ -110,15 +132,19 @@ const slide = (slideSelector) => {
         
         //show slide
         const currentSlide = slides[currentSlideIndex];
+        //load title from data-title in the header 
         title.textContent = currentSlide.dataset.title;
         currentSlide.classList.add('show');
     }
 
+    //render menu buttons
     function showButtons() {
         currentSlideIndex === 0 ? btnPrev.classList.add('btn-hide') : btnPrev.classList.remove('btn-hide');
         currentSlideIndex+1 >= slides.length ? btnNext.classList.add('btn-hide') : btnNext.classList.remove('btn-hide');
     }
 
+    //function to execute after each slide is rendered
+    //it accepts a callback (optional)
     function afterEachSlide(cb) {
         showCurrentActiveSlide();
         showButtons();
@@ -128,7 +154,9 @@ const slide = (slideSelector) => {
         }
     }
 
+    //add listeners when library loads
     addEventListenersToControls();
-    afterEachSlide();
 
+    //render for the first time
+    afterEachSlide();
 };
